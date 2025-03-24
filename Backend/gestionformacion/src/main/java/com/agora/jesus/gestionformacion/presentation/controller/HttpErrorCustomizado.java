03f90aa6781ad79f4249ab7a6806d36534637f3d
Jesús <jesusleondiaz07@gmail.com>
2025-03-24 15:56:05 +0100

    Clases de gestion de Errores

--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/presentation/controller/HttpErrorCustomizado.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/presentation/controller/HttpErrorCustomizado.java
@@ -2,4 +2,14 @@ package com.agora.jesus.gestionformacion.presentation.controller;
 
 public class HttpErrorCustomizado {
 
+	private String error;
+	
+	public HttpErrorCustomizado(String error) {
+		this.error = error;
+	}
+	
+	public String getError() {
+		return error;
+	}
+	
 }
--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/presentation/controller/PresentationException.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/presentation/controller/PresentationException.java
@@ -1,5 +1,24 @@
 package com.agora.jesus.gestionformacion.presentation.controller;
 
-public class PresentationException {
+import org.springframework.http.HttpStatus;
+
+public class PresentationException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	private HttpStatus httpStatus;
+	
+	public PresentationException(String mensaje, HttpStatus httpStatus) {
+		super(mensaje);
+		this.httpStatus = httpStatus;
+	}
+	
+	public PresentationException(String mensaje, int httpStatus) {
+		super(mensaje);
+		this.httpStatus = HttpStatus.valueOf(httpStatus);
+	}
+	
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
 
 }
