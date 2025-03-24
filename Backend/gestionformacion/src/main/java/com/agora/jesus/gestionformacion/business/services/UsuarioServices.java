b3dd7a5eb7d007ea0442255aefe1fbf73e79d159
Jesús <jesusleondiaz07@gmail.com>
2025-03-24 13:27:10 +0100

    Services terminado

--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/services/UsuarioServices.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/services/UsuarioServices.java
@@ -1,5 +1,9 @@
 package com.agora.jesus.gestionformacion.business.services;
 
+import com.agora.jesus.gestionformacion.business.model.Usuario;
+
 public interface UsuarioServices {
+	
+	Long registro(Usuario usuario);
 
 }
