fbb8825b62e1d34a8631637d2f9017ac782a30ca
Jesús <jesusleondiaz07@gmail.com>
2025-03-24 13:34:36 +0100

    Service IMPL y Repository finalizados (Sin pruebas)

--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/services/impl/UsuarioServicesImpl.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/services/impl/UsuarioServicesImpl.java
@@ -1,7 +1,34 @@
 package com.agora.jesus.gestionformacion.business.services.impl;
 
+import com.agora.jesus.gestionformacion.business.model.Usuario;
 import com.agora.jesus.gestionformacion.business.services.UsuarioServices;
+import com.agora.jesus.gestionformacion.integration.UsuarioRepository;
 
 public class UsuarioServicesImpl implements UsuarioServices {
+	
+	UsuarioRepository usuarioRepository;
 
+	public UsuarioServicesImpl(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	public Long registro(Usuario usuario) {
+		
+		if(usuario.getId() != null){
+			throw new IllegalStateException("Para crear un usuario el id tiene que ser nulo.");
+		}
+		
+		if(usuarioRepository.findByEmail(usuario.getEmail()) == null) {
+			throw new IllegalStateException("El email ya existe porfavor introduzca otro.");
+		}
+		
+		Usuario user = usuarioRepository.save(usuario);
+		
+		return user.getId();
+		
+	}
+
+	
+	
 }
--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/integration/UsuarioRepository.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/integration/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.agora.jesus.gestionformacion.integration;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.agora.jesus.gestionformacion.business.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Usuario findByEmail(String email);
+	
 }
