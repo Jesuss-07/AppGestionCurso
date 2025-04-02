d1d3752a5f07fd43c3c69274ce31d4687e88dbd3
Jesús <jesusleondiaz07@gmail.com>
2025-04-02 12:48:53 +0200

    Arreglo de la clase curso

--- Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/model/Curso.java
+++ Backend/gestionformacion/src/main/java/com/agora/jesus/gestionformacion/business/model/Curso.java
@@ -1,10 +1,15 @@
 package com.agora.jesus.gestionformacion.business.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -31,6 +36,13 @@ public class Curso {
 	private String nombre;
 	
 	private String descripcion;
+	
+	@ManyToMany
+	@JoinTable(
+	    name = "curso_usuario",
+	    joinColumns = @JoinColumn(name = "id_curso"),
+	    inverseJoinColumns = @JoinColumn(name = "id_usuario"))
+	private List<Usuario> usuarios;
 	
 	@Column(name = "numero_horas")
 	private Integer numeroHoras;
