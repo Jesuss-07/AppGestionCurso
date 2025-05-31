f25d45994f1c5eb3b55340d38ad4133ebf60f7c8
Jesús <jesusleondiaz07@gmail.com>
2025-05-31 04:17:59 +0200

    otros componentes como routes config

--- Frontend/GestionCurso/src/app/app.routes.ts
+++ Frontend/GestionCurso/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+export const routes: Routes = [
+  { path: 'register', component: RegisterUserComponent },
+];
